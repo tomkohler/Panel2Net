0ef9f88adb139ec5579eede75930ca168e669e0b
tomkohler <thkohler@gmail.com>
2017-12-31 11:05:15 +0100

    Version 1.1
    * Flushing when buffer overruns
    * Retry mechanism before switching baudrate
    * Wider range of baudrates to be covered

--- Panel2Net.py
+++ Panel2Net.py
@@ -1,10 +1,10 @@
 # Protoype for Panel2Net
 # Reads serial data and pushes it via HTTP POST onto the net
 # runs on Raspberry Pi
-# Thomas Kohler (C) 2017
+# Thomas Kohler (C) 2017/2018
+# Version 1.1 / 31-12-2017
 
 # imports
-# import threading
 import serial
 from serial import SerialException
 
@@ -16,7 +16,7 @@ import binascii
 import random
 
 # THIS IS THE UNIQUE DEVICENAME
-Device_ID = ''
+Device_ID = 'SB_ZURICH'
 
 if Device_ID == '':
     Device_ID = "SB_" + str(random.randint(1000,9999))
@@ -43,6 +43,10 @@ RequestPort = 80
 RequestURL = '/abcd/mobatime.php'
 # for an answer before aborting (seconds)
 RequestTimeOut = 10
+# Number of Retries before attempting to switch baudrate  
+ReadRetry = 3
+# MaxBuffer before flushing (in order to avoid buffer overflow)
+BufferMax = 1000
 
 # LogFileName: Name of LogFile
 LogFileName = '/tmp/Panel2Net.log'
@@ -78,7 +82,8 @@ ser.writeTimeout = 2
 
 while True:
     try:
-        print ("Initializing")
+        print ("\nInitializing")
+        ser.close()
         ser.open()
         if ser.isOpen():
             try:
@@ -88,10 +93,21 @@ while True:
                 # flush output buffer, aborting current output
                 # and discard all that is in buffer
                 RequestCount = 0
-                print ("Port Opening")
+                print ("\nPort Opening")
+                # Initialise RetryCounter
+                RetryCount = 0
         
                 while True:
+                    # If the BufferSize is larger than defined BufferMax then flush to avoid buffer overflow
+                    # print("BufferSize: " + str(ser.inWaiting()) + ", BufferMax: " + str(BufferMax))
+                    if ser.inWaiting() > BufferMax:
+                        print ("\n>>> Buffer limit exceeded. Flushing buffer")
+                        ser.flushInput()
+                        ser.flushOutput()
+
+                    # Read from Serial Interface
                     response = ser.read(PackageByLength)
+    
                     if len(response) > 0:
                         # In case there is something coming down the serial path
                         logging.debug(response)
@@ -101,93 +117,122 @@ while True:
 
                         response_hex = response.replace(b' ', b'')
                         # print("\nResponse_Hex: " + str(response_hex))
-                        
+
+                        # Evaluate if the received data is HEX or needs conversion to HEX
                         try:
                             int(response_hex,16)    
                         except ValueError:
                             # not hex, needs conversion
                             response_hex = binascii.hexlify(response)
                             response_hex = response_hex.upper()
-                            #print("\nResponse_Raw: " + str(response))    
-                            #print("\nResponse_Hex: " + str(response_hex))
+                            # print("\nResponse_Raw: " + str(response))    
+                            # print("\nResponse_Hex: " + str(response_hex))
 
+                        # Evaluate if the received data matches the panel format or not
                         if response_hex.find(b'017F0247') != -1:
-                            # if found, then mobatime
+                            # found mobatime panel data
                             # print("Mobatime: " + str(response_hex) + " - " + str(response_hex.find(b'017F0247')))
                             RequestURL = '/abcd/mobatime.php'
+                            RetryCount = 0
                         elif (response_hex.find(b'F83320') != -1) or (response_hex.find(b'E8E8E4') != -1):
-                            # stramatel with right baudrate
+                            # found stramatel panel data
                             # print("Stramatel: " + str(response_hex) + " - " + str(response_hex.find(b'F83320')))
                             RequestURL = '/abcd/stramatel.php'
+                            RetryCount = 0
                         elif (response_hex.find(b'0254') != -1) or (response_hex.find(b'0244') != -1):
-                            # if found, then SwissTiming
+                            # found SwissTiming panel data
                             # print("SwissTiming: " + str(response_hex) + " - " + str(response_hex.find(b'0244')))
                             RequestURL = '/abcd/swisstiming.php'
+                            RetryCount = 0
                         else:
-                            print("\n>>> Nothing Found, Changing Baudrate, Discarding Package")
-                            ser.close()
-                            if BaudRate == 9600:
-                                BaudRate = 19200
-                            else:
-                                BaudRate = 9600
-                            ser.baudrate = BaudRate
-                            ser.open()
-         
+                            # if not known format found, then retry as long there are retries left, otherwise change baudrate
+                            if RetryCount < ReadRetry:
+                                RetryCount += 1
+                                print("\n>>> Panel not recognized. Retry #" + str(RetryCount))
+                            else:    
+                                # Closing Serial Interface to Change Baudrate
+                                ser.close()
+                                # Switch Baudrate
+                                if BaudRate == 9600:
+                                    BaudRate = 19200
+                                elif BaudRate == 19200:
+                                    BaudRate = 38400
+                                elif BaudRate == 38400:
+                                    BaudRate = 57600
+                                elif BaudRate == 57600:
+                                    BaudRate = 115200
+                                elif BaudRate == 115200:
+                                    BaudRate = 1200
+                                elif BaudRate == 1200:
+                                    BaudRate = 2400
+                                elif BaudRate == 2400:
+                                    BaudRate = 4800
+                                elif BaudRate == 4800:
+                                    BaudRate = 9600
+                                else:
+                                    BaudRate = 9600
+
+                                # Set new Baudrate and re-open Serial Interface
+                                ser.baudrate = BaudRate
+                                ser.open()
+                                print("\n>>> Panel not recognized. Changing Baudrate to " + str(BaudRate))
+
+                            # Flush Buffers and Read to clean previous data
                             ser.flushInput()
                             ser.flushOutput()
-                            # Flush Buffers and Read Twice to clean previous data
+                            response = ser.read(PackageByLength)
                             response = ser.read(PackageByLength)
                             response = b''        
 
                         # End Evaluation Block
 
+                        if response != b'':
+                            # Make and Evaluate HTTP Request
+                            headers = {}
+                            headers['Content-type'] = 'application/x-www-form-urlencoded'
+                            headers['Accept'] = 'text/plain'
+                            headers['Content-Type'] = 'text/plain'
+                            headers['Connection'] = 'keep-alive'
+                            headers['Device_ID'] = Device_ID
 
-                        # Make and Evaluate HTTP Request
-                        headers = {}
-                        headers['Content-type'] = 'application/x-www-form-urlencoded'
-                        headers['Accept'] = 'text/plain'
-                        headers['Content-Type'] = 'text/plain'
-                        headers['Connection'] = 'keep-alive'
-                        headers['Device_ID'] = Device_ID
-
-                        conn = http.client.HTTPConnection(RequestServer,RequestPort)
-                        conn.request("POST", RequestURL, response, headers)
-                        httpreply = conn.getresponse()
-                        if httpreply.status == 200:
-                            logging.debug(str(httpreply.status) + " " + str(httpreply.reason))
-                        else:
-                            logging.error(str(httpreply.status) + " " + str(httpreply.reason))    
-                        RequestCount = RequestCount + 1    
-                        logging.debug("RequestCount: " + str(RequestCount))
-                        
-                        # Calculate End Time
-                        EnderTime = time.time() * 1000
-                        
-                        # Calculate Time used for Request Handling
-                        ElapserTime = int(EnderTime - StarterTime)
-                        print("\rRequestCount: " + str(RequestCount) + ", Baud: " + str(BaudRate) + ", Type: " + str(RequestURL) + ", Package Length: "
-                         + str (PackageByLength) + ", Handling Time: " + str(ElapserTime)
-                         + " ms -> " + str(httpreply.status), end='', flush=True)
-                        logging.debug("\rRequestCount: " + str(RequestCount) + ", Package Length: "
-                         + str (PackageByLength) + ", Handling Time: " + str(ElapserTime)
-                         + " ms -> " + str(httpreply.status))        
-                        
-                        # Adjust PackageByLength based on Handling Time
-                        if ElapserTime > 2000:
-                            PackageByLength = 4096
-                        elif ElapserTime > 1000:
-                            PackageByLength = 2048
-                        elif ElapserTime > 500:
-                            PackageByLength = 1048
-                        elif ElapserTime > 250:
-                            PackageByLength = 512
-                        elif ElapserTime > 125:
-                            PackageByLength = 256
-                        elif ElapserTime > 60:
-                            PackageByLength = 128
-                        else:
-                            PackageByLength = 128
-                        # print("New PackageLength: " + str(PackageByLength))
+                            conn = http.client.HTTPConnection(RequestServer,RequestPort)
+                            conn.request("POST", RequestURL, response, headers)
+                            httpreply = conn.getresponse()
+                            if httpreply.status == 200:
+                                logging.debug(str(httpreply.status) + " " + str(httpreply.reason))
+                            else:
+                                logging.error(str(httpreply.status) + " " + str(httpreply.reason))    
+                            RequestCount = RequestCount + 1    
+                            logging.debug("RequestCount: " + str(RequestCount))
+                            
+                            # Calculate End Time
+                            EnderTime = time.time() * 1000
+                            
+                            # Calculate Time used for Request Handling
+                            ElapserTime = int(EnderTime - StarterTime)
+                            print("\r#: " + str(RequestCount) + ", Bd: " + str(BaudRate) + ", Panel: " + str(RequestURL[6:-4]) + ", Len#: "
+                             + str (PackageByLength) + ", HT: " + str(ElapserTime)
+                             + " ms: " + str(httpreply.status) + ", Buf: " + str(ser.inWaiting()), end='       ', flush=True)
+                            logging.debug("\rRequestCount: " + str(RequestCount) + ", Package Length: "
+                             + str (PackageByLength) + ", Handling Time: " + str(ElapserTime)
+                             + " ms -> " + str(httpreply.status) + ", BufferSize: " + str(ser.inWaiting()))        
+                            
+                            # Adjust PackageByLength based on Handling Time
+                            if ElapserTime > 2000:
+                                PackageByLength = 4096
+                            elif ElapserTime > 1000:
+                                PackageByLength = 2048
+                            elif ElapserTime > 500:
+                                PackageByLength = 1048
+                            elif ElapserTime > 250:
+                                PackageByLength = 512
+                            elif ElapserTime > 125:
+                                PackageByLength = 256
+                            elif ElapserTime > 60:
+                                PackageByLength = 128
+                            else:
+                                PackageByLength = 128
+                            # print("New PackageLength: " + str(PackageByLength))
                     
                     else:
                         # In case nothing is coming down the serial interface
